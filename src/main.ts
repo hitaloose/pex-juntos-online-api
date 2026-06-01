487b20bf7b40329835603afa017404f7b1bddd4d
Hitalo Loose <hitaloose@gmail.com>
2026-06-01 19:48:16 -0300

    feat: refactor server startup to include database connection checks and migrations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { api } from './api.js';
 import { CONFIG } from './config.js';
-import { setupDb } from './utils/db.js';
+import { assertDatabaseConnectionOk, setupDb } from './utils/db.js';
 import { models } from './utils/models.js';
+import { migrator } from './utils/umzug.js';
 
-setupDb(models);
+async function start() {
+  await assertDatabaseConnectionOk();
+  await migrator.up();
 
-api.listen(CONFIG.PORT, () => {
-  console.log(`server running on ${CONFIG.PORT}`);
+  setupDb(models);
+
+  api.listen(CONFIG.PORT, () => {
+    console.log(`server running on ${CONFIG.PORT}`);
+  });
+}
+
+start().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
